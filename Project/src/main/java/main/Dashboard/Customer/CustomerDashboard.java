d8e699e9fe814c2a596ae05f3779fd4b19242069
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-06-26 00:44:19 +0430

    implement CustomerDashboard

--- Project/src/main/java/main/Dashboard/Customer/CustomerDashboard.java
+++ Project/src/main/java/main/Dashboard/Customer/CustomerDashboard.java
@@ -1,10 +1,19 @@
 package main.Dashboard.Customer;
 
 import com.jfoenix.controls.JFXButton;
+import control.Controller;
+import control.Exceptions.HaveNotLoggedInException;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import main.Main;
+import model.People.Customer;
+import view.Profile.CustomerMenu.CustomerMenuPanes;
+
+import java.io.IOException;
 
 public class CustomerDashboard {
 
@@ -34,6 +43,42 @@ public class CustomerDashboard {
 
     @FXML
     private void initialize() {
+        CustomerMenuPanes dashboard = new CustomerMenuPanes();
+        Stage stage = (Stage) stackPane.getScene().getWindow();
+        Customer customer = (Customer) Controller.getCurrentAccount();
 
+
+        usernameLabel.setText(customer.getUsername());
+
+        personalInfoButton.setOnMouseClicked(event -> {
+            stage.setScene(new Scene(dashboard.getPersonalInfoPane()));
+        });
+
+        discountCodesButton.setOnMouseClicked(event -> {
+            stage.setScene(new Scene(dashboard.getCustomerDiscountCodes(customer)));
+        });
+
+        ordersButton.setOnMouseClicked(event -> {
+            stage.setScene(new Scene(dashboard.getOrdersPane()));
+        });
+
+        homeButton.setOnMouseClicked(event -> {
+            try {
+                Main.setRoot("main");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        logoutButton.setOnMouseClicked(event -> {
+            try {
+                Controller.logout();
+                Main.setRoot("main");
+            } catch (HaveNotLoggedInException e) {
+
+            } catch (IOException e) {
+
+            }
+        });
     }
 }
