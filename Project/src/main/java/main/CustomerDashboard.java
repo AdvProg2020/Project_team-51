2d97ff7948523c15193f839501e4f41f3ce9cb72
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-06-26 00:07:38 +0430

    create Customer Dashboard controller

---
+++ Project/src/main/java/main/CustomerDashboard.java
@@ -0,0 +1,39 @@
+package main;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+
+public class CustomerDashboard {
+
+    @FXML
+    private StackPane stackPane;
+
+    @FXML
+    private BorderPane mainPane;
+
+    @FXML
+    private JFXButton homeButton;
+
+    @FXML
+    private JFXButton logoutButton;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private JFXButton personalInfoButton;
+
+    @FXML
+    private JFXButton discountCodesButton;
+
+    @FXML
+    private JFXButton ordersButton;
+
+    @FXML
+    private void initialize() {
+
+    }
+}
