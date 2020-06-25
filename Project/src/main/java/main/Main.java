97703041cae819d3e83b111e3cc6b2b55ab9896c
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-06-26 02:05:46 +0430

    add Icon and Title to Stage

--- Project/src/main/java/main/Main.java
+++ Project/src/main/java/main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import model.Database.Build;
 import model.Database.StatusUpdater;
@@ -36,6 +37,8 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("main"));
         stage.setScene(scene);
+        stage.setTitle("  JShop");
+        stage.getIcons().add(new Image(String.valueOf(Main.class.getResource("JShop.png"))));
         scene.getStylesheets().add(Main.class.getResource("main.css").toExternalForm());
         stage.show();
     }
