4994e9593bb1430df3af827e7b4144e0ea32f1c2
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-06-26 00:49:31 +0430

    improve CustomerDashboard

--- Project/src/main/java/main/Dashboard/Customer/CustomerDashboard.java
+++ Project/src/main/java/main/Dashboard/Customer/CustomerDashboard.java
@@ -1,11 +1,17 @@
 package main.Dashboard.Customer;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import com.jfoenix.controls.events.JFXDialogEvent;
 import control.Controller;
 import control.Exceptions.HaveNotLoggedInException;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -71,14 +77,58 @@ public class CustomerDashboard {
         });
 
         logoutButton.setOnMouseClicked(event -> {
-            try {
-                Controller.logout();
-                Main.setRoot("main");
-            } catch (HaveNotLoggedInException e) {
-
-            } catch (IOException e) {
-
-            }
+            BoxBlur boxBlur = new BoxBlur(4, 4, 4);
+            JFXButton button = new JFXButton("  Yes  ");
+            button.setStyle("-fx-background-color:#fe615a; -fx-background-radius:  18; -fx-text-fill: white");
+            button.setPadding(new Insets(3, 16, 3, 16));
+            JFXDialogLayout dialogLayout = new JFXDialogLayout();
+            dialogLayout.setHeading(new Label(" Logout "));
+            dialogLayout.setStyle("-fx-background-color: rgba(255,104,110,0.64)");
+            JFXDialog dialog = new JFXDialog(stackPane, dialogLayout, JFXDialog.DialogTransition.CENTER);
+            button.setOnMouseClicked((MouseEvent event1) -> {
+                dialog.close();
+            });
+            dialogLayout.setActions(button);
+            dialog.show();
+            mainPane.setEffect(boxBlur);
+            dialog.setOnDialogClosed((JFXDialogEvent event1) -> {
+                        mainPane.setEffect(null);
+                        try {
+                            Controller.logout();
+                            try {
+                                Main.setRoot("main");
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } catch (HaveNotLoggedInException e) {
+                            e.printStackTrace();
+                        }
+                    }
+            );
         });
+        
+    }
+
+    private void showError(String message) {
+        showErrorWithColor(message, "#fe615a");
+    }
+
+    @FXML
+    private void showErrorWithColor(String message, String color) {
+        BoxBlur boxBlur = new BoxBlur(4, 4, 4);
+        JFXButton button = new JFXButton("  Yes  ");
+        button.setStyle("-fx-background-color:" + color + "; -fx-background-radius:  18; -fx-text-fill: white");
+        button.setPadding(new Insets(3, 16, 3, 16));
+        JFXDialogLayout dialogLayout = new JFXDialogLayout();
+        dialogLayout.setHeading(new Label(message));
+        dialogLayout.setStyle("-fx-background-color: rgba(255,104,110,0.64)");
+        JFXDialog dialog = new JFXDialog(stackPane, dialogLayout, JFXDialog.DialogTransition.CENTER);
+        button.setOnMouseClicked((MouseEvent event) -> {
+            dialog.close();
+        });
+        dialogLayout.setActions(button);
+        dialog.show();
+        mainPane.setEffect(boxBlur);
+        dialog.setOnDialogClosed((JFXDialogEvent event) -> mainPane.setEffect(null));
     }
 }
