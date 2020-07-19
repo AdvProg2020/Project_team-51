08788c2b6529ead14602e7a4076e4fb399c10fa6
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-07-20 02:28:38 +0430

    AcceptAddSellerForItemRequestMessage

---
+++ Server/src/main/java/message/requests/AcceptAddSellerForItemRequestMessage.java
@@ -0,0 +1,16 @@
+package message.requests;
+
+import model.Requests.AddSellerForItemRequest;
+
+public class AcceptAddSellerForItemRequestMessage {
+
+    private AddSellerForItemRequest addSellerForItemRequest;
+
+    public AcceptAddSellerForItemRequestMessage(AddSellerForItemRequest addSellerForItemRequest) {
+        this.addSellerForItemRequest = addSellerForItemRequest;
+    }
+
+    public AddSellerForItemRequest getAddSellerForItemRequest() {
+        return addSellerForItemRequest;
+    }
+}
