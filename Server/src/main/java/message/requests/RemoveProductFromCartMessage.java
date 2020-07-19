6e5601da1e4130cdf8d006bf7fbc50b8db26de53
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-07-20 02:57:26 +0430

    RemoveProductFromCartMessage

---
+++ Server/src/main/java/message/requests/RemoveProductFromCartMessage.java
@@ -0,0 +1,16 @@
+package message.requests;
+
+import model.Product;
+
+public class RemoveProductFromCartMessage {
+
+    private Product product;
+
+    public RemoveProductFromCartMessage(Product product) {
+        this.product = product;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+}
