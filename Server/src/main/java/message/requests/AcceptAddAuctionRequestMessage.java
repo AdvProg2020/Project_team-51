dff3e2bdd784de8b7459077059f9c7ec49bdf461
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-07-20 02:23:14 +0430

    AcceptAddAuctionRequestMessage

---
+++ Server/src/main/java/message/requests/AcceptAddAuctionRequestMessage.java
@@ -0,0 +1,16 @@
+package message.requests;
+
+import model.Requests.AddAuctionRequest;
+
+public class AcceptAddAuctionRequestMessage {
+
+    private AddAuctionRequest addAuctionRequest;
+
+    public AcceptAddAuctionRequestMessage(AddAuctionRequest addAuctionRequest) {
+        this.addAuctionRequest = addAuctionRequest;
+    }
+
+    public AddAuctionRequest getAddAuctionRequest() {
+        return addAuctionRequest;
+    }
+}
