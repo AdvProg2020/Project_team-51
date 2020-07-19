2fb2a16787571f6084060afdc67b5826281f148a
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-07-20 02:56:09 +0430

    IncrementProductQuantityMessage & DecrementProductQuantityMessage

---
+++ Server/src/main/java/message/requests/IncrementProductQuantityMessage.java
@@ -0,0 +1,22 @@
+package message.requests;
+
+import model.ItemOfOrder;
+
+public class IncrementProductQuantityMessage {
+
+    private ItemOfOrder itemOfOrder;
+    private int number;
+
+    public IncrementProductQuantityMessage(ItemOfOrder itemOfOrder, int number) {
+        this.itemOfOrder = itemOfOrder;
+        this.number = number;
+    }
+
+    public ItemOfOrder getItemOfOrder() {
+        return itemOfOrder;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
