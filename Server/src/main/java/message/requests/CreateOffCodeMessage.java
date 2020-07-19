e8b1e3eb4bc58da5a4e92b4561881938b9ff052c
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-07-20 02:48:08 +0430

    CreateOffCodeMessage

---
+++ Server/src/main/java/message/requests/CreateOffCodeMessage.java
@@ -0,0 +1,16 @@
+package message.requests;
+
+import model.OffCode;
+
+public class CreateOffCodeMessage {
+    
+    private OffCode offCode;
+
+    public CreateOffCodeMessage(OffCode offCode) {
+        this.offCode = offCode;
+    }
+
+    public OffCode getOffCode() {
+        return offCode;
+    }
+}
