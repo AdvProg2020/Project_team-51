d7738797e82f4341cc80d9a82cd6f626a93b9720
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-07-20 02:26:17 +0430

    AcceptAddItemRequestMessage

---
+++ Server/src/main/java/message/requests/AcceptAddItemRequestMessage.java
@@ -0,0 +1,16 @@
+package message.requests;
+
+import model.Requests.AddItemRequest;
+
+public class AcceptAddItemRequestMessage {
+
+    private AddItemRequest addItemRequest;
+
+    public AcceptAddItemRequestMessage(AddItemRequest addItemRequest) {
+        this.addItemRequest = addItemRequest;
+    }
+
+    public AddItemRequest getAddItemRequest() {
+        return addItemRequest;
+    }
+}
