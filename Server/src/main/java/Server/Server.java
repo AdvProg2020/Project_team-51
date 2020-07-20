868bbb8bf873816d82c4ab02c7bec321c3a43d82
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-07-20 05:01:16 +0430

    implement receiveMessage method in Server

--- Server/src/main/java/Server/Server.java
+++ Server/src/main/java/Server/Server.java
@@ -96,6 +96,119 @@ public class Server {
     }
 
     private void receiveMessage(Message message) {
+        if (message == null) {
+            // It's a null message
+        }
+        if (!message.getReceiver().equals(serverName)) {
+            // message doesn't belong to this sever
+        }
+        switch (message.getMessageType()) {
+            case LOGIN:
+                // TODO
+                break;
+            case LOGOUT:
+                // TODO
+                break;
+            case REGISTER_CUSTOMER:
+                // TODO
+                break;
+            case REGISTER_MANAGER:
+                // TODO
+                break;
+            case REGISTER_MANAGER_BY_MANAGER:
+                // TODO
+                break;
+            case REGISTER_SERVICE_BY_MANAGER:
+                // TODO
+                break;
+            case ACCEPT_ADD_AUCTION_REQUEST:
+                // TODO
+                break;
+            case ACCEPT_ADD_COMMENT_REQUEST:
+                // TODO
+                break;
+            case ACCEPT_ADD_ITEM_REQUEST:
+                // TODO
+                break;
+            case ACCEPT_ADD_SELLER_FOR_ITEM_REQUEST:
+                // TODO
+                break;
+            case ACCEPT_ADD_SELLER_REQUEST:
+                // TODO
+                break;
+            case ACCEPT_EDIT_AUCTION_REQUEST:
+                // TODO
+                break;
+            case ACCEPT_EDIT_PRODUCT_REQUEST:
+                // TODO
+                break;
+            case CREATE_ADD_AUCTION_REQUEST:
+                // TODO
+                break;
+            case CREATE_ADD_COMMENT_REQUEST:
+                // TODO
+                break;
+            case CREATE_ADD_ITEM_REQUEST:
+                // TODO
+                break;
+            case CREATE_ADD_SELLER_FOR_ITEM_REQUEST:
+                // TODO
+                break;
+            case CREATE_ADD_SELLER_REQUEST:
+                // TODO
+                break;
+            case CREATE_EDIT_AUCTION_REQUEST:
+                // TODO
+                break;
+            case CREATE_EDIT_PRODUCT_REQUEST:
+                // TODO
+                break;
+            case CHARGE_WALLET:
+                // TODO
+                break;
+            case CREATE_BID:
+                // TODO
+                break;
+            case CREATE_CATEGORY:
+                // TODO
+                break;
+            case CREATE_FILE_FOR_SALE:
+                // TODO
+                break;
+            case CREATE_OFF_CODE:
+                // TODO
+                break;
+            case ADD_TO_CART:
+                // TODO
+                break;
+            case APPLY_OFF_CODE:
+                // TODO
+                break;
+            case BUY_FILE:
+                // TODO
+                break;
+            case DECREMENT_PRODUCT_QUANTITY:
+                // TODO
+                break;
+            case INCREMENT_PRODUCT_QUANTITY:
+                // TODO
+                break;
+            case REMOVE_PRODUCT_FROM_CART:
+                // TODO
+                break;
+            case SET_WAGE:
+                // TODO
+                break;
+            case TAKE_FROM_WALLET:
+                // TODO
+                break;
+            case PAY_CART_VIA_BANK:
+                // TODO
+                break;
+            case PAY_CART_VIA_WALLET:
+                // TODO
+                break;
+        }
 
     }
 
